2c554056c46120d37459a30321ff81406a25c684
sergeyMelentyev <sergey.melentyev@gmail.com>
2018-04-26 16:40:53 +0300

    new version

---
+++ server/db/insertDocument.js
@@ -0,0 +1,25 @@
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+const keys = require("../config/keys");
+
+const insertDocuments = (db, data, callback) => {
+    const collection = db.collection("users");
+    collection.insertOne(data)
+        .then((result, err) => {
+            assert.equal(err, null);
+            assert.equal(1, result.result.n);
+            assert.equal(1, result.ops.length);
+            callback(result);
+        });
+};
+
+module.exports = data => {
+    MongoClient.connect(keys.mongoURI, (err, client) => {
+        if (err) throw err;
+        const db = client.db("customer-satisfaction-survey");
+        insertDocuments(db, data, result => {
+            console.log(result);
+            client.close();
+        });
+    });
+};
